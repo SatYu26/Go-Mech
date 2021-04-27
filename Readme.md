201ffe09e0862b1f8158bfc0fdc21c5be87d7bc8
SatYu26 <goyalsatyam8@gmail.com>
2021-04-27 15:40:43 +0530

    update

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # to import data into mongodb
 
 mongoimport --db <Database name> --collection <collection name> --drop --file E:\PycharmProjects\go-mech\brand_model_price_sample1.json --jsonArray
+
+mongoimport --db test --collection brandsample --drop --file E:\Projects\Internship\go-mech\brand_model_price_sample1.json --jsonArray
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from prettyprinter import pprint  # to pretty print the cursor result.
 import pymongo
 
 client = pymongo.MongoClient('localhost', 27017)
@@ -10,11 +9,13 @@ brandsample = db.brandsample
 
 # pprint(db.brand.find({'variantId': "2132"}))
 # dataRequired = brand.find_one({'variantId': "2132"}).limit(1)
-dataRequired = brand.find_one({'variantId': "2132"}, {'_id': 0, })
+dataRequired = brand.find({}, {'_id': 0, 'variantId': 0, 'd2IExcellent': 0, 'd2IGood': 0, 'd2IFair': 0, 'i2DExcellent': 0, 'i2DGood': 0, 'i2DFair': 0, 'i2IExcellent': 0,
+                               'i2IGood': 0, 'i2IFair': 0, 'priceExcellent': 0, 'priceGood': 0, 'onePrice': 0, 'makeId': 0, 'modelId': 0, 'year': 0, 'owner': 0, 'kmDriven': 0, 'seller_type': 0, 'city_name': 0, })
 
-myquery = {"variantId": "2132"}
-newvalues = {"$set": dataRequired}
+for x in dataRequired:
+    myquery = {"variantId": '2132'}   # ask question
+    newvalues = {"$set": x}
 
+    brandsample.update_many(myquery, newvalues)
 
-brandsample.update_many(myquery, newvalues)
-print("Success")
+    print("Success")
